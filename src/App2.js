6829c4e631da1f191692eb4205b3814ea76e9284
chinmayd1 <chinmyay.minskole@gmail.com>
2025-10-01 20:52:38 +0530

    added

---
+++ src/App2.js
@@ -0,0 +1,15 @@
+
+// class based component ---
+// function based component---
+
+import React from "react";
+
+function App2(){
+    return(
+        <div>
+           <h1>hello</h1>
+           <h1>bye</h1> 
+        </div>
+    )
+}
+export default App2
---
+++ src/Welcome.js
@@ -0,0 +1,8 @@
+import React from "react";
+function Welcome(){
+    return(
+        <h1>Welcome to react class</h1>
+    )
+}
+
+export default Welcome
